5be1001941bb4dcdc9d1f1679cf9c87f010870cd
AthulyaDominic <athulyadominic2002@gmail.com>
2026-06-17 14:50:21 +0530

    Excel report creation added

--- vehicle_rental/wizard/rental_report_wizard.py
+++ vehicle_rental/wizard/rental_report_wizard.py
@@ -42,27 +42,90 @@ class RentalReportWizard(models.TransientModel):
             'vehicle_rental.action_rental_request_report'
         ).report_action([], data=data)
 
+
     def action_generate_excel(self):
 
-            output = io.BytesIO()
+        output = io.BytesIO()
 
-            workbook = xlsxwriter.Workbook(output)
-            worksheet = workbook.add_worksheet('Rental Report')
+        workbook = xlsxwriter.Workbook(output)
+        worksheet = workbook.add_worksheet('Rental Report')
 
-            worksheet.write('A1', 'Rental Request Report')
+        # Title
+        worksheet.write('A1', 'Rental Request Report')
 
-            worksheet.write('A3', 'Rental ID')
-            worksheet.write('B3', 'Customer')
-            worksheet.write('C3', 'Request Date')
-            worksheet.write('D3', 'Rent Date')
-            worksheet.write('E3', 'Return Date')
-            worksheet.write('F3', 'Period')
-            worksheet.write('G3', 'Status')
+        # Headers
+        worksheet.write('A3', 'Rental ID')
+        worksheet.write('B3', 'Customer')
+        worksheet.write('C3', 'Request Date')
+        worksheet.write('D3', 'Rent Date')
+        worksheet.write('E3', 'Return Date')
+        worksheet.write('F3', 'Period')
+        worksheet.write('G3', 'Status')
 
-            workbook.close()
+        # Fetch records
+        vehicle_id=self.vehicle_id.id
+        from_date=self.from_date
+        to_date=self.to_date
 
-            output.seek(0)
+        query="""SELECT *,
+        (SELECT name FROM res_partner WHERE id=rental_request.customer_id)
+        AS customer_name
+        FROM rental_request WHERE 1=1 """
+
+        if vehicle_id:
+            query+= f" AND vehicle_id={vehicle_id}"
+        if from_date:
+            query+= f" AND request_date>='{from_date}'"
+        if to_date:
+            query+= f" AND request_date<='{to_date}'"
+
+        self.env.cr.execute(query)
+        rows=self.env.cr.dictfetchall()
+
+        # Data starts after header row
+        row = 3
+
+        for rec in rows:
+            worksheet.write(row, 0, rec['rental_id'])
+            worksheet.write(row, 1, rec['customer_name'])
+            worksheet.write(row, 2, str(rec['request_date']))
+            worksheet.write(row, 3, str(rec['rent_date']))
+            worksheet.write(row, 4, str(rec['return_date']))
+            worksheet.write(row, 5, rec['period'])
+            worksheet.write(row, 6, rec['status'])
+
+            row += 1
+
+        # Close workbook LAST
+        workbook.close()
+
+        output.seek(0)
+
+        print("Excel Button Clicked")
+        print("Excel Headers Created")
+        print("Excel Data Written")
 
 
-            print("Excel Button Clicked")
-            print("Excel Headers Created")
+        #read the file and convert it to encoded format
+        import base64
+
+        excel_data=output.read()
+        excel_file=base64.b64encode(excel_data)
+
+        print(type(excel_file))
+        print(excel_file[:50])
+
+        #create an attachment in odoo
+        attachment=self.env['ir.attachment'].create({
+            'name':'Rental_Report.xlsx',
+            'type':'binary',
+            'datas':excel_file,
+            'mimetype':'application/vnd.openxmlformat-officedocument.spreadsheetml.sheet'
+        })
+
+        #Return download action
+        return{
+            'type':'ir.actions.act_url',
+            'url':f'/web/content/{attachment.id}?download=true',
+            'target':'self'
+        }
